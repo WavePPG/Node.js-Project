8955e0011185dfcc386d0b5e2f95e8032b2f93f2
WavePPG <65160143@go.buu.ac.th>
2024-05-11 14:29:23 +0700

    Add all file of Project
    Hello wold :+1:

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+const port = 3000;
+const debug = require('debug')('app')
+
+app.use(morgan('combined'))
+app.get("/" , (req,res) =>{
+
+    res.send('Hello borntodev');
+
+}) //จัดการกับ Reques และส่งคำว่า Hello borntodev กลับไป
+
+app.listen(port, ()=>{
+    debug("listening on port %d",port);
+})
